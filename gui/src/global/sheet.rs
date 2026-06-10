d60fda53aae1c76a4461e0ae8aea1a894ca89b5d
omochikaeri15 <251449262+omochikaeri15@users.noreply.github.com>
2026-06-10 06:45:23 -0400

    removed redundant comments

--- gui/src/global/sheet.rs
+++ gui/src/global/sheet.rs
@@ -10,15 +10,11 @@ pub struct GuiSpriteSheet {
 
 impl GuiSpriteSheet {
     pub fn load(&mut self, png_path: &Path, imgcut_path: &Path, id_str: String) {
-        // Forward the load request to the background thread in the core
         self.core.load(png_path, imgcut_path, id_str);
     }
 
     pub fn update(&mut self, ctx: &egui::Context) {
-        // Tick the core to see if the thread finished loading the raw image
         self.core.update();
-
-        // If the core has the image data, but we haven't uploaded it to the GPU yet:
         if self.texture_handle.is_none() && !self.core.is_loading_active
             && let Some(image_data) = &self.core.image_data {
                 let size = [image_data.width() as usize, image_data.height() as usize];
